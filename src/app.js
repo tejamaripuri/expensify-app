dbf9981e5dd4dbc606fe792e1adca26007266f66
Teja <teja.maripuri@gmail.com>
2018-03-07 19:42:43 -0600

    Added React Routers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-ReactDOM.render(<p>My Boilerplate</p> , document.getElementById('app'));
+
+ReactDOM.render(<AppRouter /> , document.getElementById('app'));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = (props) => (
+    <div>
+        Editing the {props.match.params.id}.
+    </div>
+);
+
+export default EditExpensePage;
