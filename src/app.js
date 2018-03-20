b31e45b0887461f583fc3d1eb840a5255444d429
Teja <teja.maripuri@gmail.com>
2018-03-20 10:46:07 -0500

    Added Redux

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
 
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//     console.log(visibleExpenses);
+// });
 
-ReactDOM.render(<AppRouter /> , document.getElementById('app'));
+store.dispatch(addExpense({description: 'Water Bill', amount: 4500}));
+store.dispatch(addExpense({description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 1700000}));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}> 
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx , document.getElementById('app'));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
+import ExpenseListFilters from '../components/ExpenseListFilters'
 
 const ExpenseDashboardPage = () => (
     <div>
-        This is from my dashboard component.
+        <ExpenseListFilters />
+        <ExpenseList />
     </div>
 );
 
